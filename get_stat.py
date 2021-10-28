0ac7f53efcb5d1f34bef83c284b0b1c1390c7e51
pbochkarev <pbochkarev@ussc.ru>
2021-10-29 00:17:43 +0500

    Добавил работу с аргументом и прописал README.md

--- get_stat.py
+++ get_stat.py
@@ -1,11 +1,11 @@
 import re
 import json
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='Logs parser')
-parser.add_argument('--path', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-
+parser.add_argument('--path', help='Путь к файлу или папке с логами')
+args = parser.parse_args()
 
 request_pattert = re.compile(r'(?<=\")(\w+)(?= )')
 ip_pattern = re.compile(r'^\d+\.\d+\.\d+\.\d+')
@@ -13,19 +13,32 @@ request_url_pattern = re.compile(r'(?<=\")(.*?)(?=\")')
 time_pattern = re.compile(r'\d+$')
 
 
-def get_string():
+def get_string_some_files():
     '''
-    возвращает объект генератор для работы с большим файлом логов
+    возвращает объект генератор для работы с большим файлом логов,
+    при работе с директорией логов
     '''
-    with open("./access.log", "r") as logs:
+    for file in files:
+        with open(f'{args.path}/{file}', "r") as logs:
+            for string in logs:
+                yield string
+
+
+def get_string_single_files():
+    '''
+    возвращает объект генератор для работы с большим файлом логов,
+    при работе с одним файлом
+    '''
+    with open(f'{args.path}', "r") as logs:
         for string in logs:
             yield string
 
 
-logs = get_string()
-ip_dict = {}
-request_dict = {}
-time_dict = {}
+try:
+    files = os.listdir(args.path)
+    logs = get_string_some_files()
+except NotADirectoryError:
+    logs = get_string_single_files()
 
 
 def different_elements_counter(log_string: str, pattern, elements_dict: dict) -> dict:
@@ -82,7 +95,8 @@ def get_requests_time(ip_patern, request_url_pattern, time_pattern, log_string, 
     :param time_dict: словарь в который складываются найденные элементы
     :param id: id, нужен для уникальности каждого ключа
     :param kwargs: передаются паттерны для формирования ключа
-    :return: возращает слварь формата {}
+    :return: возращает слварь с элементами формата
+     {"6094 213.150.254.81 GET /templates/_system/css/general.css HTTP/1.1": "9999"}
     '''
     ip = ip_patern.search(log_string)
     request_url = request_url_pattern.search(log_string)
@@ -96,29 +110,21 @@ def get_requests_time(ip_patern, request_url_pattern, time_pattern, log_string, 
 
 
 id = 0
+ip_dict = {}
+request_dict = {}
+time_dict = {}
 for string in logs:
     ip_dict = different_elements_counter(string, ip_pattern, ip_dict)
     request_dict = different_elements_counter(string, request_pattert, request_dict)
     time_dict = get_requests_time(ip_pattern, request_url_pattern, time_pattern, string, time_dict, id)
     id += 1
 
-
 result = {'Общее количество выполненных запросов': elements_counter(request_dict),
           'Количество запросов по типу': request_dict,
           'Топ 3 IP адресов, с которых были сделаны запросы': get_top3_elements(ip_dict),
           'Топ 3 самых долгих запросов': get_top3_elements(time_dict)}
 
-print(get_top3_elements(ip_dict))
-print(get_top3_elements(time_dict))
-print(elements_counter(request_dict))
-#print(elements_counter(ip_dict))
-# print(a)
-# print(len(ip_dict))
-# # print(ip_dict)
-print(request_dict)
-print(json.dumps(result, sort_keys=True, indent=4, ensure_ascii=False))
 result_json = json.dumps(result, sort_keys=True, indent=4, ensure_ascii=False)
-with open('result.json', 'w') as file:
+print(result_json)
+with open('result2.json', 'w') as file:
     json.dump(result_json, file, ensure_ascii=False, sort_keys=True, indent=4)
-
-
